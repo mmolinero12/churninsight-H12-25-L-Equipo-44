f61efbb59ac86a801a4a18921a017f208c030ddd
mmolinero12 <mmolinero12@gmail.com>
2026-01-11 09:47:25 -0600

    Proyecto desde CERO descargado desde initializr con las dependencias Dev+Lombok+Web

---
+++ src/test/java/org/hackaton/oracle/churninsight/ChurninsightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackaton.oracle.churninsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurninsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
